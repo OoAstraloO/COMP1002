3cbd3bc1a6ea5c1f7de33caf8d0b5dbcd9cd2a86
Charlie <200533963@student.georgianc.on.ca>
2025-11-02 11:37:07 -0500

    Created orderconfirmation.php
    Created .php for order confirmation with minimum information

---
+++ assignment-1/orderconfirmation.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Order Confirmation | COMP1002 The HTML Pizza Company</title>
+		<meta name="author" content="Charlie Sirois-Morin"/>
+		<meta name="description" content="Add concise description here">
+		<link rel="shortcut icon" href="images/pizzaicon.png" type="image/x-icon"/>
+		<link rel="stylesheet" href="css/styles.css"/>
+	</head>
+	<body>
+		<!-- Page-level header -->
+		<header>
+			<h1>The HTML Pizza Company</h1>
+			<h2></h2>
+		</header>
+		<!-- Page-level main content -->
+		<main>
+			<section>
+				<h3>Thank you for ordering</h3>
+
+			</section>
+		</main>
+		<!-- Page-level footer -->
+		<footer>
+			<p><small>© HTML Pizza Company Inc.</small></p>
+		</footer>
+	</body>
+</html>
