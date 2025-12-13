1d7516fc77fd56fc192bd12ce6383dd4b4c552cb
Charlie <200533963@student.georgianc.on.ca>
2025-12-13 04:14:18 -0500

    Moved logos into div

--- project-1/js/scale.js
+++ project-1/js/scale.js
@@ -1,7 +1,7 @@
 // Function which scales the entire page based on the browser window width
 function scalePage() 
 {
-    const baseWidth = 1920;
+    const baseWidth = 1024;
     // Calculates the scale ratio based on current window width
     let scale = window.innerWidth / baseWidth;
     // Stops page from zooming past 100%
